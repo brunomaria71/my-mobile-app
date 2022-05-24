819a7a884b2c0db927451e51342a259ba15ec15a
brunomaria71 <brunomaria71@yahoo.com>
2022-05-24 15:31:44 -0400

    added a counter button

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Counter from './src/components/Counter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Open up App.js to start working on your app!</Text>
+      <Button title='press me please' color="#841584" />
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  heading: {
+    fontSize: 38,
+    color: '#fff'
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+import { View, Text, Button} from 'react-native';
+
+export default function Counter() {
+
+
+
+
+    return (
+        <View>
+            <Text>You Clicked the button 0 times</Text>
+            <Button title='Click Me' /> 
+        </View>
+    )
+}
